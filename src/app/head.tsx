16b7ec573f3d0f76ee399f888689bca5de9ad94f
anirudh-tech <117503731+anirudh-tech@users.noreply.github.com>
2025-08-25 14:46:41 +0530

    Delete src directory

--- src/app/head.tsx
+++
@@ -1,9 +0,0 @@
-export default function Head() {
-	return (
-		<>
-			<link rel="icon" href="/Logo.png" />
-			<link rel="shortcut icon" href="/Logo.png" />
-			<link rel="apple-touch-icon" href="/Logo.png" />
-		</>
-	)
-}
--- src/types/react-icons.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'react-icons/fa' {
-  export const FaFacebookF: any
-  export const FaTwitter: any
-  export const FaInstagram: any
-  export const FaWhatsapp: any
-}
-
-
